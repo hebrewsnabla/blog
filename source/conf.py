28c21af60fc6a84acb870d7852ed07a5b625b45c
hebrewsnabla <wsr@smail.nju.edu.cn>
2021-07-30 20:59:37 +0800

    .

--- source/conf.py
+++ source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, wsr'
 author = 'wsr'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0'
+#release = '0.0'
 
 
 # -- General configuration ---------------------------------------------------
